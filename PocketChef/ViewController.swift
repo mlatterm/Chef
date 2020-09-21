3b5a7010baff247a417e9ef18067f9212fa47a65
Max <mlatterm@samford.edu>
2020-09-21 16:04:44 +0200

    Initial Commit

---
+++ PocketChef/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PocketChef
+//
+//  Created by Max Lattermann on 9/21/20.
+//  Copyright © 2020 Max Lattermann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PocketChefTests/PocketChefTests.swift
@@ -0,0 +1,34 @@
+//
+//  PocketChefTests.swift
+//  PocketChefTests
+//
+//  Created by Max Lattermann on 9/21/20.
+//  Copyright © 2020 Max Lattermann. All rights reserved.
+//
+
+import XCTest
+@testable import PocketChef
+
+class PocketChefTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
