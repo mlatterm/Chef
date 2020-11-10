b170242165fbc43d64f8bcafe724e3d18472533c
John C <jbc4ua@gmail.com>
2020-11-10 12:48:29 -0600

    Adding .swift files for the Recipe data model and the actual recipe(s)

---
+++ PocketChef/RecipeData.swift
@@ -0,0 +1,37 @@
+//
+//  RecipeData.swift
+//  PocketChef
+//
+//  Created by John Corley on 11/10/20.
+//  Copyright © 2020 Max Lattermann. All rights reserved.
+//
+
+import SwiftUI
+
+// MARK: - RECIPE DATA
+
+let recipesData: [Recipe] = [
+Recipe (
+  title: "John’s Dank Egg Salad Recipe",
+  headline: "egg salad has never been easier!",
+  image: "egg-saladSandwich",
+  rating: 5,
+  serves: 4,
+  prepTime: 30,
+  cookTime: 16,
+  instructions: [
+  "After boiling (~10 min) and shelling 8 eggs, separate the yolk from the white, smush it up with a fork, mix all the following stuff with the yolk, then add it back with the chopped white and resume mixing. (feel free garnish it with more paprika when serving it)"
+  ],
+  ingredients: [
+    "1 cup mayonnaise",
+    "1.5 teaspoon vinegar",
+    "~2 tablespoons of chopped green onion",
+    "2 teaspoons of dill relish",
+    "2.5 teaspoon Dijon mustard",
+    "2/3 teaspoon salt",
+    "1/2 teaspoon pepper",
+    "1.5 pinch of parsley",
+    "A couple dashes of paprika"
+  ]
+)
+]
