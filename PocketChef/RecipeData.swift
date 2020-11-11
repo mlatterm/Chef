09fa93a8986c3ecb690400105b658f9b26fe7656
John C <jbc4ua@gmail.com>
2020-11-10 18:06:56 -0600

    formatted (for looks) recipe data

--- PocketChef/RecipeData.swift
+++ PocketChef/RecipeData.swift
@@ -11,27 +11,27 @@ import SwiftUI
 // MARK: - RECIPE DATA
 
 let recipesData: [Recipe] = [
-Recipe (
-  title: "John’s Dank Egg Salad Recipe",
-  headline: "Delicious egg salad made easy!",
-  image: "egg-saladSandwich",
-  rating: 5,
-  serves: 5,
-  prepTime: 30,
-  cookTime: 16,
-  instructions: [
-  "After boiling (~10 min) and shelling 8 eggs, separate the yolk from the white, smush it up with a fork, mix all the following stuff with the yolk, then add it back with the chopped white and resume mixing. (feel free garnish it with more paprika when serving it)"
-  ],
-  ingredients: [
-    "1 cup mayonnaise",
-    "1.5 teaspoon vinegar",
-    "~2 tablespoons of chopped green onion",
-    "2 teaspoons of dill relish",
-    "2.5 teaspoon Dijon mustard",
-    "2/3 teaspoon salt",
-    "1/2 teaspoon pepper",
-    "1.5 pinch of parsley",
-    "A couple dashes of paprika"
-  ]
-)
-]
+    Recipe (
+      title: "John’s Dank Egg Salad Recipe",
+      headline: "Delicious egg salad made easy!",
+      image: "egg-saladSandwich",
+      rating: 5,
+      serves: 5,
+      prepTime: 30,
+      cookTime: 16,
+      instructions: [
+      "After boiling (~10 min) and shelling 8 eggs, separate the yolk from the white, smush it up with a fork, mix all the following stuff with the yolk, then add it back with the chopped white and resume mixing. (feel free garnish it with more paprika when serving it)"
+      ],
+      ingredients: [
+        "1 cup mayonnaise",
+        "1.5 teaspoon vinegar",
+        "~2 tablespoons of chopped green onion",
+        "2 teaspoons of dill relish",
+        "2.5 teaspoon Dijon mustard",
+        "2/3 teaspoon salt",
+        "1/2 teaspoon pepper",
+        "1.5 pinch of parsley",
+        "A couple dashes of paprika"
+      ]
+    )
+    ]
