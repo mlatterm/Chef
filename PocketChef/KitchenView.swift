ae69311ca9dff7bc9bd7575b981efec84bf6fcd7
Max <mlatterm@samford.edu>
2020-11-16 18:36:59 +0100

    kitchenView

---
+++ PocketChef/KitchenView.swift
@@ -0,0 +1,15 @@
+//
+//  KitchenView.swift
+//  PocketChef
+//
+//  Created by Max Lattermann on 11/16/20.
+//  Copyright © 2020 Max Lattermann. All rights reserved.
+//
+
+import SwiftUI
+import CoreData
+
+class KitchenView: UIViewController{
+    
+}
+
